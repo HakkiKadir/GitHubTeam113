373a284b2c5c107b5a5c5ed2e75773a6c765242b
HakkiKadir <hakkikadirozbek@gmail.com>
2023-03-28 23:08:03 +0300

    ilkDers

---
+++ src/gitHub_İlkDers.java
@@ -0,0 +1,7 @@
+public class gitHub_İlkDers {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("=============");
+        System.out.println("Have wise a day");
+    }
+}
